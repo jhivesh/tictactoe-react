dd8edb637a9c7871f397d3979169107b6a09fbab
jhivesh <46216085+jhivesh@users.noreply.github.com>
2020-10-22 11:08:24 +0400

    Add files via upload

---
+++ Game.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import './App.css';
+import Board from "./Components/Board";
+import "./index.css"; 
+
+class Game extends Component {
+  render() {
+    return (
+      <div className="game">
+      <div className="game-board">
+        <Board />
+      </div>
+      <div className="game-info">
+        <div>{/* status */}</div>
+        <ol>{/* TODO */}</ol>
+      </div>
+    </div>
+        
+    )
+  }
+}
+
+export default Game;
